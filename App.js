3b6d2c2e5f62a22ab1b941cac50fa5f87a5e9e54
Pablo Villarreal <pablovillarreal@PablosMacStudio.lan>
2025-01-13 13:05:20 -0600

    initial nav  skeleton

--- App.js
+++ App.js
@@ -1,22 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import WeatherScreen from './screens/WeatherScreen';
+import AlertsScreen from './screens/AlertsScreen';
+import NewsScreen from './screens/NewsScreen';
 
-
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Weather" component={WeatherScreen} />
+        <Tab.Screen name="Alerts" component={AlertsScreen} />
+        <Tab.Screen name="News" component={NewsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ screens/NewsScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function WeatherScreen() {
+  return (
+    <View>
+      <Text>News Information</Text>
+    </View>
+  );
+}
